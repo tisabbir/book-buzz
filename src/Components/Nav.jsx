89306f3272116c4e038d211c850b776675f28ab5
TI Sabbir <tisabbir0@gmail.com>
2024-03-27 07:21:49 +0600

    Nav section made responsive

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,10 @@ const Nav = () => {
     <div>
       <div className="navbar flex justify-between items-center">
         <div className="">
-          <Link to={"/"} className="btn btn-ghost text-3xl font-bold">
+          <Link
+            to={"/"}
+            className="btn btn-ghost text-xl lg:text-3xl font-bold"
+          >
             Book Buzz
           </Link>
         </div>
@@ -15,8 +18,8 @@ const Nav = () => {
               to={"/"}
               className={({ isActive }) =>
                 isActive
-                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-xl"
-                  : "btn btn-ghost font-semibold text-xl"
+                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-md lg:text-xl"
+                  : "btn btn-ghost font-semibold text-md lg:text-xl"
               }
             >
               Home
@@ -25,8 +28,8 @@ const Nav = () => {
               to={"/listed"}
               className={({ isActive }) =>
                 isActive
-                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-xl"
-                  : "btn btn-ghost font-semibold text-xl"
+                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-md lg:text-xl"
+                  : "btn btn-ghost font-semibold text-md lg:text-xl"
               }
             >
               Listed Books
@@ -35,17 +38,17 @@ const Nav = () => {
               to={"/pages"}
               className={({ isActive }) =>
                 isActive
-                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-xl"
-                  : "btn btn-ghost font-semibold text-xl"
+                  ? "btn btn-ghost border border-[#23BE0A] text-[#23BE0A] font-semibold text-md lg:text-xl"
+                  : "btn btn-ghost font-semibold text-md lg:text-xl"
               }
             >
               Pages
             </NavLink>
           </ul>
         </div>
-        <div className="flex gap-4 ">
-          <button className="btn bg-[#23BE0A] text-white">SignIn</button>
-          <button className="btn bg-[#59C6D2] text-white">SignUp</button>
+        <div className="hidden lg:flex gap-4 ">
+          <button className="btn bg-[#23BE0A] text-white">Sign In</button>
+          <button className="btn bg-[#59C6D2] text-white">Sign Up</button>
         </div>
       </div>
     </div>
